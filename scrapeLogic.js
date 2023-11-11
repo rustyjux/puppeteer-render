8ba001030bb0503aa8d62f1a7657160ae13f507a
Russell Vinegar <russell.vinegar@gmail.com>
2023-11-11 10:40:51 -0800

    more simple screenshot

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -18,8 +18,10 @@ const scrapeLogic = async (res) => {
     // Capture the screenshot
     const page = await browser.newPage();
     const timeout = 10000;
-    await page.goto("https://nordic-pulse.com/ski-areas/CA/BC/Black-Jack-Ski-Club");
-    await page.setViewport({width: 2160, height: 1920});
+    await page.goto("https://www.example.org/");
+    // await page.goto("https://nordic-pulse.com/ski-areas/CA/BC/Black-Jack-Ski-Club");
+    // await page.setViewport({width: 2160, height: 1920});
+    await page.setViewport({ width: 1080, height: 1024 });
     await waitTillHTMLRendered(page)
     console.log(`Capturing screenshot`)
     // console.log(`Capturing screenshot - dark mode? ${darkMode}`)
@@ -72,8 +74,8 @@ const scrapeLogic = async (res) => {
     //     await delay(2000);
     // }
     
-    const map = await page.$('body > app-root > div > app-ski-area > div > div > div');
-    const screenshot = await map.screenshot();
+    // const map = await page.$('body > app-root > div > app-ski-area > div > div > div');
+    const screenshot = await page.screenshot();
     console.log('Returning screenshot')
     res.send(screenshot);
 } catch (e) {
