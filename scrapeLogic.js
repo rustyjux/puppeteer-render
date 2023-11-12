62b69c6a8ca29c654c28aaef48639ee5fe9ae12d
Russell Vinegar <russell.vinegar@gov.bc.ca>
2023-11-12 15:22:49 -0800

    capture map, darkMode, no timeout

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -17,67 +17,71 @@ const scrapeLogic = async (res, darkMode) => {
   try {
     // Capture the screenshot
     const page = await browser.newPage();
-    const timeout = 30000;
+    page.setDefaultNavigationTimeout(0); // TODO - set high but not unlimited
+    const timeout = 0; // TODO - set high but not unlimited
+    // const timeout = 30000;
     // await page.goto("https://www.example.org/");
+    console.log(`Loading page`)
     await page.goto("https://nordic-pulse.com/ski-areas/CA/BC/Black-Jack-Ski-Club", {timeout:0});
     await page.setViewport({width: 2160, height: 1920});
     // await page.setViewport({ width: 1080, height: 1024 });
-    console.log(`Loading page`)
+    console.log('Waiting for page to load')
+
     
     await waitTillHTMLRendered(page)
     console.log(`Capturing screenshot`)
     console.log(`Capturing screenshot - dark mode? ${darkMode}`)
-    // if (darkMode === 'true') {
-    //     {
-    //         const targetPage = page;
-    //         await puppeteer.Locator.race([
-    //             targetPage.locator('div.map-settings > button'),
-    //             targetPage.locator('::-p-xpath(/html/body/app-root/div/app-ski-area/div/div/div/npl-map-libre/div/div/div[3]/button)'),
-    //             targetPage.locator(':scope >>> div.map-settings > button')
-    //         ])
-    //             .setTimeout(timeout)
-    //             .click({
-    //               offset: {
-    //                 x: 15,
-    //                 y: 14,
-    //               },
-    //             });
-    //     }
-    //     {
-    //         const targetPage = page;
-    //         await puppeteer.Locator.race([
-    //             targetPage.locator('::-p-aria(Dark) >>>> ::-p-aria([role=\\"image\\"])'),
-    //             targetPage.locator('button:nth-of-type(2) > ion-img >>>> img'),
-    //             targetPage.locator(':scope >>> button:nth-of-type(2) > ion-img >>>> :scope >>> img')
-    //         ])
-    //             .setTimeout(timeout)
-    //             .click({
-    //               offset: {
-    //                 x: 31,
-    //                 y: 28,
-    //               },
-    //             });
-    //     }
-    //     {
-    //         const targetPage = page;
-    //         await puppeteer.Locator.race([
-    //             targetPage.locator('div.map-settings > div > span'),
-    //             targetPage.locator('::-p-xpath(/html/body/app-root/div/app-ski-area/div/div/div/npl-map-libre/div/div/div[3]/div/span)'),
-    //             targetPage.locator(':scope >>> div.map-settings > div > span')
-    //         ])
-    //             .setTimeout(timeout)
-    //             .click({
-    //               offset: {
-    //                 x: 13,
-    //                 y: 15,
-    //               },
-    //             });
-    //     }
-    //     await delay(2000);
-    // }
+    if (darkMode === 'true') {
+        {
+            const targetPage = page;
+            await puppeteer.Locator.race([
+                targetPage.locator('div.map-settings > button'),
+                targetPage.locator('::-p-xpath(/html/body/app-root/div/app-ski-area/div/div/div/npl-map-libre/div/div/div[3]/button)'),
+                targetPage.locator(':scope >>> div.map-settings > button')
+            ])
+                .setTimeout(timeout)
+                .click({
+                  offset: {
+                    x: 15,
+                    y: 14,
+                  },
+                });
+        }
+        {
+            const targetPage = page;
+            await puppeteer.Locator.race([
+                targetPage.locator('::-p-aria(Dark) >>>> ::-p-aria([role=\\"image\\"])'),
+                targetPage.locator('button:nth-of-type(2) > ion-img >>>> img'),
+                targetPage.locator(':scope >>> button:nth-of-type(2) > ion-img >>>> :scope >>> img')
+            ])
+                .setTimeout(timeout)
+                .click({
+                  offset: {
+                    x: 31,
+                    y: 28,
+                  },
+                });
+        }
+        {
+            const targetPage = page;
+            await puppeteer.Locator.race([
+                targetPage.locator('div.map-settings > div > span'),
+                targetPage.locator('::-p-xpath(/html/body/app-root/div/app-ski-area/div/div/div/npl-map-libre/div/div/div[3]/div/span)'),
+                targetPage.locator(':scope >>> div.map-settings > div > span')
+            ])
+                .setTimeout(timeout)
+                .click({
+                  offset: {
+                    x: 13,
+                    y: 15,
+                  },
+                });
+        }
+        await delay(2000);
+    }
     
     const map = await page.$('body > app-root > div > app-ski-area > div > div > div');
-    const screenshot = await page.screenshot();
+    const screenshot = await map.screenshot();
     console.log('Returning screenshot')
     res.setHeader('Content-Type', 'image/png');
     res.send(screenshot);
